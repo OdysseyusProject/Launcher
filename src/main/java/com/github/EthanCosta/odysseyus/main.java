351722324f7c46e12dda0e146411486b8183295d
TATHAN <costa.tathan@gmail.com>
2022-02-13 17:16:01 +0100

    Update some things
    Signed-off-by: TATHAN <costa.tathan@gmail.com>

--- src/main/java/com/github/EthanCosta/odysseyus/main.java
+++ src/main/java/com/github/EthanCosta/odysseyus/main.java
@@ -13,6 +13,7 @@ public class main {
     public static void main(String[] args) {
         try {
             Class.forName("javafx.application.Application");
+            //C VRAI
             System.out.println("Made by Tathan and bibi_fire because i suck in java");
             Application.launch(Launcher.class, args);
 
